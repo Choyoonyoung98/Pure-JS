4941142316245c3b34925258a395bdbfee775605
조윤영 <yoon980208@gmail.com>
2019-01-30 16:11:21 +0900

    dom study finished

--- crash/dom/dom.js
+++ crash/dom/dom.js
@@ -147,17 +147,88 @@ var itemlist = document.querySelector('#items');
 
 // container.insertBefore(newDiv,h1);
 
-var button = document.getElementById('button').addEventListener('click', buttonClick)
+// var button = document.getElementById('button').addEventListener('click', buttonClick)
 
-function buttonClick(e) {
+// function buttonClick(e) {
 // document.getElementById('header-title').textContent = 'Changed';
 // document.querySelector('#main').style.backgroundColor = '#f4f4f4';
 // console.log(e);
 
-console.log(e.target);
-//dom crash course part3 6:17
+// console.log(e.target);
+// console.log(e.target.id);
+// console.log(e.target.className);
+// console.log(e.target.classList);
+
+// console.log(e.type);//what kind of type event is
+
+// console.log(e.clientX);//client is from window
+// console.log(e.clientY)
+
+// console.log(e.offsetX); //actual element of iteself
+// console.log(e.offsetY);
+
+// console.log(e.altKey);
+// console.log(e.ctrlKey);
+// console.log(e.shiftKey);
+
+
+// }
+
+var button = document.getElementById('button');
+var box = document.getElementById('box');
+
+// button.addEventListener('click', runEvent);
+// button.addEventListener('dbclick', runEvent);
+// button.addEventListener('mousedown', runEvent);
+// button.addEventListener('mouseup', runEvent);//when I release clicked status
+
+// box.addEventListener('mouseenter',runEvent);//only act in exactly at id
+// box.addEventListener('mouseleave',runEvent);
+
+// box.addEventListener('mouseover',runEvent);//act also in children element
+// box.addEventListener('mouseout',runEvent);
+
+// box.addEventListener('mousemove',runEvent);
+
+var output = document.getElementById('output');
+
+var itemInput = document.querySelector('input[type="text"]');
+var form = document.querySelector('form');
+var select = document.querySelector('select');
+
+// itemInput.addEventListener('keydown',runEvent);
+// itemInput.addEventListener('keyup',runEvent);//if keydown let go
+// itemInput.addEventListener('keypress',runEvent);
+
+// itemInput.addEventListener('focus',runEvent);
+// itemInput.addEventListener('blur',runEvent);//not focus
+
+// itemInput.addEventListener('cut',runEvent);
+// itemInput.addEventListener('paste',runEvent);
+
+// itemInput.addEventListener('input',runEvent);//anything you do input
+// select.addEventListener('change',runEvent);
+
+form.addEventListener('submit',runEvent);
+
+function runEvent(e) {
+  e.preventDefault();
+  console.log('EVENT TYPE: '+e.type);
+  // itemInput.value =e.target.value;
+
+
+
+  // console.log(e.target.value);//can catch what I type
+  // document.getElementById('output').innerHTML = '<h3>'+e.target.value+'</h3>';
+
+
+  // output.innerHTML = '<h3>MouseX: '+e.offsetX+'</h3><h3>MouseY: '+e.offsetY+'</h3>';
+
+  // document.body.style.backgroundColor = "rgb("+e.offsetX+","+e.offsetY+",40"+")";
 
 }
+
+
 
 
 
