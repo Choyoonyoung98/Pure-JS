e7ff89ace70b6659a45177dd489af99a9c6a6d27
조윤영 <yoon980208@gmail.com>
2019-01-30 17:04:07 +0900

    dom project -ing

---
+++ crash/dom/domLittle.js
@@ -0,0 +1,46 @@
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+
+//Form submit event
+form.addEventListener('submit', addItem);
+//Delete event
+itemList.addEventListener('click'.removeItem);
+
+//Add item
+function addItem(e) {
+  e.preventDefault();
+
+  //Get Input  value
+  var newItem = document.getElementById('item').value;
+
+  //Create new li element
+  var li = document.createElement('li');
+  //Add class
+  li.className = 'list-group-item';
+  //Add text node with input value
+  li.appendChild(document.createTextNode(newItem));
+
+  //Create del button element
+  var deleteBtn = document.createElement('button');
+
+  //Add classes to del button
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+  //Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  //Append button to li
+  li.appendChild(deleteBtn);
+
+  //Append li to list
+  itemList.appendChild(li);
+
+
+}
+
+  //Remove item
+  function removeItem(e) {
+    console.log(1);
+    //dom pro part4 11:50
+
+  }
