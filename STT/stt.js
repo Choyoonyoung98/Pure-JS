793a8b753c0eb1c20d8b4f3cf7a8581ae0e92d9d
조윤영 <yoon980208@gmail.com>
2019-01-31 19:52:03 +0900

    stt demo

--- STT/stt.js
+++ STT/stt.js
@@ -1,61 +1,78 @@
 
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
-var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+//Add grammar
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
 
-var colors = [ 'aqua' , 'azure' , 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
-var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
+var keywords = ['고양이','강아지','전미경','조영서'];
+var grammar = '#JSGF V1.0; grammar keywords; public <keyword> = ' + keywords.join(' | ') + ' ;'
 
 var recognition = new SpeechRecognition();
+
+//Add grammar
 var speechRecognitionList = new SpeechGrammarList();
 speechRecognitionList.addFromString(grammar, 1);
 recognition.grammars = speechRecognitionList;
-//recognition.continuous = false;
-recognition.lang = 'en-US';
+
+recognition.lang = 'ko-KR';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
+
+var voice= ""
+
 var diagnostic = document.querySelector('.output');
-var bg = document.querySelector('html');
-var hints = document.querySelector('.hints');
 
-var colorHTML= '';
-colors.forEach(function(v, i, a){
-  console.log(v, i);
-  colorHTML += '<span style="background-color:' + v + ';"> ' + v + ' </span>';
-});
-hints.innerHTML = 'Tap/click then say a color to change the background color of the app. Try '+ colorHTML + '.';
+var start = document.getElementById('start');
+start.addEventListener('click', onstart);
 
-document.body.onclick = function() {
+function onstart() {
   recognition.start();
-  console.log('Ready to receive a color command.');
+  console.log('Ready to receive a command.');
 }
 
+
 recognition.onresult = function(event) {
-  // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
-  // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
-  // It has a getter so it can be accessed like an array
-  // The [last] returns the SpeechRecognitionResult at the last position.
-  // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
-  // These also have getters so they can be accessed like arrays.
-  // The [0] returns the SpeechRecognitionAlternative at position 0.
-  // We then return the transcript property of the SpeechRecognitionAlternative object
 
   var last = event.results.length - 1;
-  var color = event.results[last][0].transcript;
 
-  diagnostic.textContent = 'Result received: ' + color + '.';
-  bg.style.backgroundColor = color;
+
+  voice = voice + event.results[last][0].transcript;
+
+  diagnostic.textContent = 'Result received: ' + voice + '.';
   console.log('Confidence: ' + event.results[0][0].confidence);
+
+  addElement();
+
+
+}
+function addElement() {
+
+  for(i = 0; i < keywords.length; i++) {
+    if (voice.includes(keywords[i])){
+
+      var keywordList = document.getElementById('keywordList');
+
+      //Create new li element
+      var keyword = document.createElement('span');
+      //Add class
+      keyword.className = 'btn btn-primary';
+      //Add text node with input value
+      keyword.appendChild(document.createTextNode(keywords[i]));
+
+      //Append li to list
+      keywordList.appendChild(keyword);
+    }
+  }
+
 }
 
 recognition.onspeechend = function() {
   recognition.stop();
 }
-
-recognition.onnomatch = function(event) {
-  diagnostic.textContent = "I didn't recognise that color.";
-}
+recognition.onnomatch = event => {
+  console.log('no match');
+};
 
 recognition.onerror = function(event) {
   diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
